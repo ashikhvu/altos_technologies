5db643796ecf207771c0aa32cb30d8930432586a
ashikhvu <vuashikh16@gmail.com>
2023-12-11 16:14:06 +0000

    initial commit

--- Altots/settings.py
+++ Altots/settings.py
@@ -83,9 +83,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'infoxtec_altosdbtesting',
-        'USER':'infoxtec_altosdbtesting',
-        'PASSWORD':'altosdbtesting',
-        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
     }
 }
@@ -133,7 +133,10 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
 
 # Default primary key field type
